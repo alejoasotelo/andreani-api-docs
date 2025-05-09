a062ed0b12987a3c12d2d1e0317185557ddb8211
Alejo Sotelo <alesotelo@gmail.com>
2025-05-09 16:26:05 -0300

    [*] documentación funcionando

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -2,12 +2,12 @@ import * as OpenAPI from 'fumadocs-openapi';
 import { rimraf } from 'rimraf';
 
 export async function generateDocs() {
-  await rimraf('./content/docs/openapi/(generated)');
+  await rimraf('./content/docs/*', { glob: { ignore: ['./content/docs/index.mdx', './content/docs/andreani.yaml'] } });
 
   await Promise.all([
     OpenAPI.generateFiles({
-      input: ['./content/docs/openapi/andreani.yaml'],
-      output: './content/docs/openapi/(generated)',
+      input: ['./content/docs/andreani.yaml'],
+      output: './content/docs/',
       per: 'operation',
       structurePath: {
         // Cambia la estructura de directorio para que todos los endpoints estén en la raíz
